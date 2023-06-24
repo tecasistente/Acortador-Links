e562044f6834795a4eb7a0cf14af12bab9026ea5
tecasistente <127995706+tecasistente@users.noreply.github.com>
2023-06-24 16:05:50 -0600

    Agregar comentario y readme

--- Acortador_URL.py
+++ Acortador_URL.py
@@ -1,31 +1,34 @@
-import streamlit as st
-import pyshorteners
-shorter = pyshorteners.Shortener()
+import streamlit as st  # Importar la biblioteca Streamlit para crear la aplicación web
+import pyshorteners  # Importar la biblioteca pyshorteners para acortar y expandir URLs
 
+shorter = pyshorteners.Shortener()  # Instanciar un objeto Shortener para utilizar sus métodos
 
-st.title("Acortador de URL")
-form=st.form("URL",clear_on_submit=True)
-url=form.text_input("Colocar la URL para acortar:")
-button=form.form_submit_button("Acortar")
+st.title("Acortador de URL") 
 
-with st.sidebar:
-    st.write("Herramienta para cortar o expandir un link usando tinyurl")
+# Crear un formulario para acortar URLs
+form = st.form("URL", clear_on_submit=True)
+url = form.text_input("Colocar la URL para acortar:") 
+button = form.form_submit_button("Acortar") 
 
-if button: #funcionar solo cuando el botton es presionado
+# Crear una barra lateral en la interfaz de la aplicación
+with st.sidebar: 
+    st.write("Herramienta para cortar o expandir un link usando tinyurl") 
+
+if button: 
     try:
-        url_shorter=shorter.tinyurl.short(url)
-        st.success("El link acortado es: "+ url_shorter)
+        url_shorter = shorter.tinyurl.short(url)           # Acortar la URL utilizando el método short()
+        st.success("El link acortado es: " + url_shorter)  
     except:
-        st.error("Algo salió mal :(")
+        st.error("Algo salió mal :(")  
 
-form_expand=st.form("URL2", clear_on_submit=True)
-url_new=form.text_input("Colocar la URL acortada: ")
-button_expand=form.form_submit_button("Expandir")
-if button_expand:
+# Crear un formulario para expandir URLs acortadas
+form_expand = st.form("URL2", clear_on_submit=True)
+url_new = form.text_input("Colocar la URL acortada: ") 
+button_expand = form.form_submit_button("Expandir")
+
+if button_expand:  # Verificar si se presionó el botón de envío
     try:
-        url_expand=shorter.tinyurl.expand(url_new)
-        st.success(" El link acortado es: "+ url_expand)
+        url_expand = shorter.tinyurl.expand(url_new)  # Expandir la URL acortada utilizando el método expand()
+        st.success("El link expandido es: " + url_expand)  
     except:
-        st.error("Algo salió mal :(")
-    
-    
+        st.error("Algo salió mal :(") 
---
+++ README.md
@@ -0,0 +1,26 @@
+
+# Acortador de Links
+
+Aplicación web en Streamlit para acortar y expandir URLs utilizando el servicio TinyURL.
+
+
+
+
+## Instalación
+
+Para instalar las dependencias:
+
+```bash
+  pip install -r requirements.txt
+```
+    
+## Ejecución del programa
+Para ejecutar el programa:
+```bash
+  streamlit run Acortador_URL.py
+```
+En el navegador ir a:
+```bash
+  http://localhost:8501
+
+```
